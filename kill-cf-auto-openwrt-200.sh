062af37c3d63e397152fe0619f64df9c0cd38c2f
coolhotsz <45020610+coolhotsz@users.noreply.github.com>
2021-09-19 07:53:03 +0800

    Update kill-cf-auto-openwrt-200.sh

--- kill-cf-auto-openwrt-200.sh
+++ kill-cf-auto-openwrt-200.sh
@@ -1,2 +1,2 @@
-#杀openwrt cf脚本进程，防止死循环，也设定时任务，在启动cf脚本后自定义时间启动删除如8点半退行CF脚本。9点杀进程，0 9 * * * bash /kill-cf-auto-openwrt-200.sh（路径自己改）
+#杀openwrt cf脚本进程，防止死循环，也设定时任务，在启动cf脚本后自定义时间启动删除如8点半退行CF脚本。9点杀进程，0 9 * * * bash /root/kill-cf-auto-openwrt-200.sh（路径自己改）
 ps -ef | grep cf-auto-openwrt-200.sh | grep -v grep | xargs kill -9
